3ee371b3042218f0a58f32bb87d8290f64515663
cavy24 <vakulenko2410@mail.ru>
2018-07-23 10:43:31 +0300

    Initial the project

---
+++ server/server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let logger = require('morgan');
+let mongoose = require('mongoose');
+
+let routeComments = require('./routes/commentRoute');
+
+//mongoose.connect('mongo://localhost:27017/comments');
+
+let app = new express;
